a82438a9695ac632c13ac0535775fdfd493b8408
jnyjhow <jhonatan_yamane@hotmail.com>
2022-09-06 12:38:43 -0300

    tutorial 05, 06 e 07 conforme issue #5

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+# incluindo a classe escolha dentro de question
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    # variaveis para visualizacao em /admin/polls/question
+    # list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
